8bb053c36ae09eefb4cf064175580e9be2dbd9c8
ggm77 <shmshm86@naver.com>
2026-07-03 14:17:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CAMPON - backend
---
+++ src/main/java/com/seohamin/campon/CamponApplication.java
@@ -0,0 +1,13 @@
+package com.seohamin.campon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamponApplication.class, args);
+	}
+
+}
